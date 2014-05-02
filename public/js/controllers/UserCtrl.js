22df600163447ba0d5bcc8920bfbb7f415845013
farshid1 <s.f.hamidi@gmail.com>
2014-05-01 19:44:35 -0700

    Revert "Revert "delete files""
    This reverts commit 1b8ed82c2d1e33be547ccb27281f60d4cc216212.

--- public/js/controllers/UserCtrl.js
+++ public/js/controllers/UserCtrl.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 angular.module('UserCtrl', ['UserService']).controller('UserController', ['$window','$location', 'UserService',
 
     function ($window, $location, UserService) {
@@ -5,12 +6,14 @@ angular.module('UserCtrl', ['UserService']).controller('UserController', ['$wind
     	//$scope.user = {};
         //$rootScope.isLoggedin = false;
         
+=======
 angular.module('UserCtrl', ['UserService']).controller('UserController', ['$scope',  'UserService',
 
     function ($scope, UserService) {
 
     	$scope.user = {};
 
+>>>>>>> FETCH_HEAD
     	$scope.submit = function(formData) {
     		console.log($scope.user);
     		$scope.master = angular.copy(formData);
@@ -18,13 +21,16 @@ angular.module('UserCtrl', ['UserService']).controller('UserController', ['$scop
     		UserService.postData($scope.master)
     		.then (
     			function(r) {
+<<<<<<< HEAD
 				console.log(r.data.role);
                 $rootScope.role = r.data.role;
 
+=======
 				console.log(r.data);
                 
 				//$scope.restaurants = angular.copy(r.data);
 				//alert('data loaded');
+>>>>>>> FETCH_HEAD
 			},
 			function (r) {
 				alert('failed')
